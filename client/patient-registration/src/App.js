7e423a9ddfef9847285125508f9ebcec1d53532f
Yash-Raj Vekaria <yvekaria@uwo.ca>
2025-02-10 21:06:54 -0500

    Fixed app.js for better navigation - will added protect routes later

--- client/patient-registration/src/App.js
+++ client/patient-registration/src/App.js
@@ -5,6 +5,8 @@ import GroupRegistrationForm from './PatientRegistration/GroupRegistrationForm';
 import Homepage from './Homepage';
 import EditFamilyGroups from './FamilyGroups/EditFamilyGroups';
 import HealthQuestionnaire from './PatientRegistration/HealthQuestionnaire';
+import VerifyEmail from './Auth/VerifyEmail';
+import Login from './Auth/Login';
 
 const App = () => (
   <Router>
@@ -14,6 +16,8 @@ const App = () => (
       <Route path="/groupregister" element={<GroupRegistrationForm />} />
       <Route path="/edit-family-group" element={<EditFamilyGroups />} />
       <Route path="/health-questionnaire/:patientId" element={<HealthQuestionnaire />} />
+      <Route path="/verify-email/:token" element={<VerifyEmail />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   </Router>
 );
